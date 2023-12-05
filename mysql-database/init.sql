4a21f0b84bbef3b9bbc3140d82935f80bd9b09c7
Gorun <307532500@qq.com>
2023-12-05 21:50:56 +0800

    添加字符集设置

--- mysql-database/init.sql
+++ mysql-database/init.sql
@@ -1,12 +1,12 @@
-drop schema if exists test;
+﻿drop schema if exists test;
 create schema test;
 use test;
 
 create table db1(
     id int primary key
 );
-insert into db1 values (1);
 
 create table db2(
     name varchar(20) primary key
 );
+insert into db2 values ('test'), ('测试');
